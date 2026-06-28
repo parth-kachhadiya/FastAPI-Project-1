0f635fface77cb8db713854351a9b514287d2394
parth-kachhadiya <parthkachhadiya04@gmail.com>
2026-06-28 21:19:01 +0530

    Added helper URLs

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+
+"""
+FastAPI endpoint : http://localhost:8000/
+
+FastAPI metrics  : http://localhost:8000/metrics
+
+Prometheus UI    : http://localhost:9090/
+    Run query : http_requests_total
+
+Grafana          : http://localhost:3000
+    Username : admin
+    Password : admin
+"""
+
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 from app.api import route_auth, route_predict
@@ -18,3 +32,4 @@ Instrumentator().instrument(app).expose(app)
 
 # Add exception handler
 registerExceptionHandlers(app)
+
